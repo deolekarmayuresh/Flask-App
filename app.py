a3690b2807a1f45a868a7497b8eec1c73988ac55
mayuresh deolekar <mayureshdeolekar.md@gmail.com>
2020-10-18 00:05:40 +0530

    adding comments

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask
 app = Flask(__name__)
 
-# template
+# Template
 @app.route('/')
 def hello_world():
     return '<h1>Hello, World!</h1>'
 
+# debugger mode
 if (__name__) == "__main__":
     app.run(debug=True)
 
