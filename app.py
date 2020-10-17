416294d05cfdbf7647da9fb9623c1859bcf664db
mayuresh deolekar <mayureshdeolekar.md@gmail.com>
2020-10-18 00:07:49 +0530

    adding routes

--- app.py
+++ app.py
@@ -6,6 +6,11 @@ app = Flask(__name__)
 def hello_world():
     return '<h1>Hello, World!</h1>'
 
+@app.route('/about')
+def about():
+    return '<h1>About Me</h1>'
+
+
 # debugger mode
 if (__name__) == "__main__":
     app.run(debug=True)
